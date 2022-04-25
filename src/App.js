acb3d65620411fec269ec43fa3b2fc2c19228c55
student <jimmy@bluewiz.net>
2022-04-25 15:27:37 -0700

    thing

--- src/App.js
+++ src/App.js
@@ -4,32 +4,62 @@ import sampleData from './sampleData';
 import StockList from './StockList';
 
 function App() {
-  
+
   // Uncomment setMyName if required, for example, if the name
   // is stored in the DynamoDB
   const [myName/*, setMyName*/] = useState('Roger');
-  
+
+  const AWS_API_GATEWAY = 'https://gor5c0brhk.execute-api.us-east-1.amazonaws.com/prod/';
+  const AWS_API_GATEWAY_GET_PORTFOLIO = AWS_API_GATEWAY + '/get-portfolio';
+
   const [stocks, setStocks] = useState([]);
   const [stockList, setStockList] = useState([]);
-  
+
   // Retrieve the current stock information when the page first loads
-  useEffect(() => {
-    setStocks(sampleData);
-  }, []);
+  const options = {
+    method: 'POST',
+    cache: 'default'
+  };
+
+  fetch(AWS_API_GATEWAY_GET_PORTFOLIO, options)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      let stockList = response.Items.map(item => {
+        return {
+          "name": item.name.S,
+          "ticker": item.ticker.S,
+          "purchasePrice": item.purchasePrice.N,
+          "shares": item.shares.N
+        };
+      });
+    
+      setStocks(stockList)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
   
+
   // With the stock data add purchase value, current price
   // and current value to the stock record
   useEffect(() => {
     const enhancedStocks = stocks.map(stock => {
       stock.purchaseValue = stock.shares * stock.purchasePrice;
-      stock.currentPrice = Math.random()*200 + 50;
+      stock.currentPrice = Math.random() * 200 + 50;
       stock.currentValue = stock.shares * stock.currentPrice;
       stock.profit = stock.currentValue - stock.purchaseValue;
       return stock;
     })
     setStockList(enhancedStocks);
   }, [stocks])
-  
+
   const addStock = evt => {
     console.log('add stock clicked');
   }
